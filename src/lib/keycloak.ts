78bc4a96b51a59286ceb9a0d32ff369a88876e23
FedericoSerini <federico.serini.se@pec.it>
2026-06-03 16:47:12 +0200

    Switch Keycloak realm from master to homelab

--- src/lib/keycloak.ts
+++ src/lib/keycloak.ts
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
   url: import.meta.env.VITE_KEYCLOAK_URL as string,
-  realm: 'master',
+  realm: 'homelab',
   clientId: 'moneybud',
 })
 
